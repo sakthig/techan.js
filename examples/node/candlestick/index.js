506732c833bf05f5ef3fcef08ef3ec8ebdf11d6a
André Dumas <adumas@odecee.com.au>
2015-05-20 22:20:53 +1000

    Added jsdom dependency and initialisation since it was removed as dependency of d3

--- examples/node/candlestick/index.js
+++ examples/node/candlestick/index.js
@@ -8,8 +8,9 @@ var chart = require('./chart');
 
 var csvData = d3.csv.parse(fs.readFileSync('./data.csv', { encoding: 'utf-8' }).trim());
 
+var document = require('jsdom').jsdom();
 // Create the chart, passing in runtime environment specific setup: node d3, techan and csv data
-var body = d3.select("body").call(chart(d3, techan, csvData));
+var body = d3.select(document.body).call(chart(d3, techan, csvData));
 
 // Output result AVG
 console.log('<?xml version="1.0" encoding="utf-8"?>');
